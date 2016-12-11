db195e985287f93e717b614fb64a3b1ca1a58c0f
jpowleytxd <jorden.powley@txdlimited.co.uk>
2016-12-11 21:57:57 +0000

    Starting writing Template builder

--- branded_belly_band.php
+++ branded_belly_band.php
@@ -1,7 +1,21 @@
+<?php
+
 /*
+Do Once: Load Image Bespoke Block
 
-
-
-
-
+For All:
+  Load Branded Template
+  Remove Styles At Top
+  Insert String into String Between Content Markers
+  Save To File
 */
+
+$imageBlock = file_get_contents("flares/_defaults/image.html");
+
+foreach (glob("*/templates/*.html") as $filename) {
+  $template = file_get_contents($filename);
+
+  $template = preg_replace('/\{.*?\}/ms', '', $template);
+}
+
+ ?>
